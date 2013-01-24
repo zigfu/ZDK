534eee2a3d753c7437af575b34a68029bbc14d89
Amir Hirsch <amir@tinkerheavy.com>
2013-01-23 22:34:54 -0800

    adding facetrackingwrapper.h

--- facetracking/facetrackingwrapper.h
+++ facetracking/facetrackingwrapper.h
@@ -1,1 +1,17 @@
+#define EXPORT_API __declspec(dllexport)
+
 #include <FaceTrackLib.h>
+
+extern "C"
+{
+		 IFTFaceTracker * EXPORT_API CreateFaceTracker()
+		 {
+			return FTCreateFaceTracker();
+		 }
+
+		 HRESULT EXPORT_API FT_Initialize(IFTFaceTracker * pFT, const FT_CAMERA_CONFIG * pVideoCameraConfig, const FT_CAMERA_CONFIG *pDepthCameraConfig, FTRegisterDepthToColor depthToColorMappingFunc, PCWSTR pszModelPath)
+		 {
+			 return pFT->Initialize(pVideoCameraConfig, pDepthCameraConfig, depthToColorMappingFunc, pszModelPath);
+		 }
+
+}
